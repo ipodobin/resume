202bb83273d6aa2f5868f28eefc1843eea3b459e
ipodobin <ipodobin@gmail.com>
2019-02-22 15:01:02 +0100

    - fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import {ResumeComponent} from './resume/resume.component';
 const appRoutes: Routes = [
   // {path: '', pathMatch: 'full', redirectTo: '/'},
   // {path: '/:lang?', component: ResumeComponent},
-  {path: '', component: ResumeComponent},
+  // {path: '', component: ResumeComponent},
   {path: ':name', component: ResumeComponent},
   // {path: '/edit/:lang?', component: ResumeComponent}
   // {path: ':mode', component: ResumeComponent}
